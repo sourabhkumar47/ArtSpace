f5dee3175fe01f9df8d47b42f4ab6d0245fb2847
Sourabh Kumar <20010001030@gateway.edu.in>
2022-12-05 22:14:16 +0530

    Added rounded corner

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,11 +42,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ArtSpace() {
     Column(
-        modifier = Modifier.padding(32.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        modifier = Modifier.padding(16.dp)
     ) {
         image()
         Spacer(Modifier.height(16.dp))
+
+        TextBox(R.string.title)
+        TextBox(R.string.subHeading)
 
     }
 
@@ -54,13 +62,33 @@ fun image(
         painter = painterResource(id = R.drawable.pexels_james_wheeler_1598073),
         contentDescription = null,
         modifier
+            .clip(
+                shape = RoundedCornerShape(12.dp)
+            )
             .fillMaxWidth()
     )
 }
 
 @Composable
-fun TextViewComp(){
+fun TextBox(
+    @StringRes label: Int
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
+        Text(
+            text = stringResource(label),
+            fontSize = 24.sp
+        )
+
+//        Text(
+//            text = stringResource(R.string.subHeading),
+//            fontSize = 15.sp
+//        )
+    }
 
 }
 
